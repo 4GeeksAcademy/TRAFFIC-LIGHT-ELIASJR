b7754c741e71ef36cd9286f6695e5186995d40e0
Elías JR <167987056+eliasjr89@users.noreply.github.com>
2024-08-11 21:37:08 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
-// Corrección en index.js
 import React from "react";
 import ReactDOM from "react-dom";
-import "../styles/index.css"; // Asegúrate de que la ruta sea correcta
-import Home from "./component/home.jsx"; // Verifica la ruta del componente Home
+import "../styles/index.css";
+import Home from "./component/home.jsx";
 
 ReactDOM.createRoot(document.getElementById('app')).render(<Home />);
